b4d48e25ecf813460f5d30bfde09dc9b1319b3d0
michaeljamesrojas <michaeljamesrojas123@gmail.com>
2023-05-18 22:24:55 +0800

    add cebuano voices

--- playSound.py
+++ playSound.py
@@ -6,5 +6,22 @@
 # play_obj.wait_done()
 # print("haha")
 
-from playsound import playsound
-playsound('Pfifty.wav')
+
+
+# from playsound import playsound
+# playsound('Pfifty.wav')
+
+
+# from pydub import AudioSegment
+# from pydub.playback import play
+
+# song = AudioSegment.from_wav("Pfifty.wav")
+# play(song)
+
+
+
+import winsound
+winsound.PlaySound("Pfifty.wav", winsound.SND_FILENAME)  
+print("haha")
+# add winsound.SND_ASYNC flag if you want to wait for it. 
+# like winsound.PlaySound("Wet Hands.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
